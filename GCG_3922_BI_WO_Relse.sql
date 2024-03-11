aee6ea64450b19c20a314751a39b3da58da2f284
Service Web <sw@gssmail.com>
2024-03-11 08:54:11 -0500

    Commit changes in C:\Websites\Git\emarlow\3922\GCG_3922_BI_WO_Relse.sql

---
+++ GCG_3922_BI_WO_Relse.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_3922_BI_WO_Relse"(
+ "BIRUNID" BIGINT,
+ "Job_Suffix" CHAR(10),
+ "Part" CHAR(20),
+ "Part_Desc" CHAR(30),
+ "Print_FLag" CHAR(1),
+ "Labor_Sequence" CHAR(6),
+ "Date_Start" DATE);
